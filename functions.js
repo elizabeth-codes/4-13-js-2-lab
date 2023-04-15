3ab1d084aa6c2b89be6beb4d9e9ffdf5f7f08aa4
Elizabeth Joyce <ekj28@cornell.edu>
2023-04-14 18:07:38 -0700

    write calculate total and get full name functions

--- functions.js
+++ functions.js
@@ -6,9 +6,22 @@ return town ==== "San Francisco"
 
 // 2. getFullName
 
-// Define your function here
+return (`${first_name} ${last_name}`);
 
 // 3. calculateTotal
 
-// Define your function here
+let subtotal = base_price * (1 + tax)
+
+let fee = 0
+    if state === "CA":
+        let fee = 0.03 * subtotal
+    elif state === "PA":
+        let fee === 2
+    elif state === "MA":
+        if base_price <= 100:
+            let fee = 1
+        else:
+            let fee = 3
+
+    return subtotal + fee
    
